13b8c57e4ed0920caff113e0b60f9f78d2c91638
Chris Hudson <c.ahudson84@yahoo.com>
2017-06-01 08:05:13 -0700

    updates to the html for step1 submission.

--- pyramid_learning_journal/pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/pyramid_learning_journal/views/default.py
@@ -1,40 +1,38 @@
 """Views for learning journal."""
-from pyramid.view import view_config
-from pyramid_learning_journal.data.data import posts
-from pyramid.httpexceptions import HTTPNotFound
+from pyramid.response import Response
+import os
+import io
+
+HERE = os.path.dirname(__file__)
 
 
-@view_config(route_name='home', renderer='../templates/list.jinja2')
 def list_view(request):
     """Import page for main.html."""
-    return {'page': 'home', 'posts': posts}
+    with io.open(os.path.join(HERE, '../templates/main.html')) as the_file:
+        imported_page = the_file.read()
+
+    return Response(imported_page)
 
 
-@view_config(route_name='detail', renderer='../templates/entry.jinja2')
 def detail_view(request):
     """Import page for entry.html."""
-    the_id = int(request.matchdict['id'])
-    try:
-        post = posts[the_id]
-    except IndexError:
-        raise HTTPNotFound
-    return {'page': 'detail', 'title': post['title'],
-            'date': post['creation_date'], 'body': post['body']}
+    with io.open(os.path.join(HERE, '../templates/entry.html')) as the_file:
+        imported_page = the_file.read()
+
+    return Response(imported_page)
 
 
-@view_config(route_name='create', renderer='../templates/new_entry.jinja2')
 def create_view(request):
     """Import page for new_entry.html."""
-    return {'page': 'create'}
+    with io.open(os.path.join(HERE, '../templates/new_entry.html')) as the_file:
+        imported_page = the_file.read()
+
+    return Response(imported_page)
 
 
-@view_config(route_name='edit', renderer='../templates/edit_entry.jinja2')
 def edit_view(request):
     """Import page for edit_entry.html."""
-    the_id = int(request.matchdict['id'])
-    try:
-        post = posts[the_id]
-    except IndexError:
-        raise HTTPNotFound
-    return {'page': 'edit', 'title': post['title'],
-            'date': post['creation_date'], 'body': post['body']}
+    with io.open(os.path.join(HERE, '../templates/edit_entry.html')) as the_file:
+        imported_page = the_file.read()
+
+    return Response(imported_page)
