ac16f1d7d36f2c4f665e61d0c7eee048d1649e31
Chris Hudson <c.ahudson84@yahoo.com>
2017-06-02 14:47:01 -0700

    finished testing and such, ready for pull request.

--- pyramid_learning_journal/pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/pyramid_learning_journal/views/default.py
@@ -1,39 +1,44 @@
 """Views for learning journal."""
-from pyramid.response import Response
 from pyramid.view import view_config
-import os
-import io
-
-HERE = os.path.dirname(__file__)
+from pyramid_learning_journal.data.data import posts
+from pyramid.httpexceptions import HTTPNotFound
 
 
+@view_config(route_name='home', renderer='../templates/main.jinja2')
 def list_view(request):
-    """Import page for main.html."""
-    with io.open(os.path.join(HERE, '../templates/main.html')) as the_file:
-        imported_page = the_file.read()
-
-    return Response(imported_page)
+    """View for the home page with list of entries."""
+    return {'page': 'home', "posts": posts}
 
 
+@view_config(route_name='detail', renderer='../templates/entry.jinja2')
 def detail_view(request):
-    """Import page for entry.html."""
-    with io.open(os.path.join(HERE, '../templates/entry.html')) as the_file:
-        imported_page = the_file.read()
+    """View to see an individual entry."""
+    the_id = int(request.matchdict['id'])
+    entry = None
+    for item in posts:
+        if item['id'] == the_id:
+            entry = item
+            break
+    if entry is None:
+        raise HTTPNotFound
+    return {'page': 'detail', 'entry': entry}
 
-    return Response(imported_page)
 
-
+@view_config(route_name='create', renderer='../templates/new_entry.jinja2')
 def create_view(request):
-    """Import page for new_entry.html."""
-    with io.open(os.path.join(HERE, '../templates/new_entry.html')) as the_file:
-        imported_page = the_file.read()
-
-    return Response(imported_page)
+    """View for adding a new entry."""
+    return {'page': 'create'}
 
 
+@view_config(route_name='edit', renderer='../templates/edit_entry.jinja2')
 def edit_view(request):
-    """Import page for edit_entry.html."""
-    with io.open(os.path.join(HERE, '../templates/edit_entry.html')) as the_file:
-        imported_page = the_file.read()
-
-    return Response(imported_page)
+    """View for editing an entry."""
+    the_id = int(request.matchdict['id'])
+    entry = None
+    for item in posts:
+        if item['id'] == the_id:
+            entry = item
+            break
+    if entry is None:
+        raise HTTPNotFound
+    return {'page': 'edit', 'entry': entry}
