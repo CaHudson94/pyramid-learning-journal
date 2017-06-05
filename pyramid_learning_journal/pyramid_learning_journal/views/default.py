5da66734ccba6fc9e62f91973db0a54bae15cd3f
Chris Hudson <c.ahudson84@yahoo.com>
2017-06-05 08:30:08 -0700

    finished reverting files from accidental update to step2.

--- pyramid_learning_journal/pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/pyramid_learning_journal/views/default.py
@@ -1,44 +1,35 @@
 """Views for learning journal."""
+from pyramid.response import Response
 from pyramid.view import view_config
-from pyramid_learning_journal.data.data import posts
-from pyramid.httpexceptions import HTTPNotFound
+import os
+import io
+
+HERE = os.path.dirname(__file__)
 
 
-@view_config(route_name='home', renderer='../templates/main.jinja2')
 def list_view(request):
     """Open home page with list of entries."""
-    return {'page': 'home', "posts": posts}
+    with io.open(os.path.join(HERE, '../templates/main.html')) as the_file:
+        imported_page = the_file.read()
+    return Response(imported_page)
 
 
-@view_config(route_name='detail', renderer='../templates/entry.jinja2')
 def detail_view(request):
     """Open individual entry page."""
-    the_id = int(request.matchdict['id'])
-    entry = None
-    for item in posts:
-        if item['id'] == the_id:
-            entry = item
-            break
-    if entry is None:
-        raise HTTPNotFound
-    return {'page': 'detail', 'entry': entry}
+    with io.open(os.path.join(HERE, '../templates/entry.html')) as the_file:
+        imported_page = the_file.read()
+    return Response(imported_page)
 
 
-@view_config(route_name='create', renderer='../templates/create.jinja2')
 def create_view(request):
     """Open new entry page."""
-    return {'page': 'create'}
+    with io.open(os.path.join(HERE, '../templates/new_entry.html')) as the_file:
+        imported_page = the_file.read()
+    return Response(imported_page)
 
 
-@view_config(route_name='edit', renderer='../templates/edit_entry.jinja2')
 def edit_view(request):
     """Open edit entry page."""
-    the_id = int(request.matchdict['id'])
-    entry = None
-    for item in posts:
-        if item['id'] == the_id:
-            entry = item
-            break
-    if entry is None:
-        raise HTTPNotFound
-    return {'page': 'edit', 'entry': entry}
+    with io.open(os.path.join(HERE, '../templates/edit_entry.html')) as the_file:
+        imported_page = the_file.read()
+    return Response(imported_page)
