beb18b121bdffc26c22d810be0b65918c9f782f3
Chris Hudson <c.ahudson84@yahoo.com>
2017-06-05 07:32:41 -0700

    actual update to tests

--- pyramid_learning_journal/pyramid_learning_journal/test_pyramid_learning_journal.py
+++ pyramid_learning_journal/pyramid_learning_journal/test_pyramid_learning_journal.py
@@ -2,21 +2,87 @@
 from pyramid import testing
 from pyramid_learning_journal.data.data import posts
 import pytest
+import transaction
+from pyramid_learning_journal.models import (
+    Entry,
+    get_tm_session,
+)
+from pyramid_learning_journal.models.meta import Base
+from pyramid_learning_journal.views.default import (
+    list_view,
+    create_view,
+    detail_view,
+    edit_view
+)
 
 
 @pytest.fixture
 def testapp():
     """Create a test application to use for functional tests."""
-    from pyramid_learning_journal import main
     from webtest import TestApp
+    from pyramid.config import Configurator
+    import os
+
+    def main(global_config, **settings):
+        """Function returns a Pyramid WSGI application."""
+        settings['sqlalchemy.url'] = os.environ.get('TEST_DATABASE')
+        config = Configurator(settings=settings)
+        config.include('pyramid_jinja2')
+        config.include('.models')
+        config.include('.routes')
+        config.add_static_view(name='static', path='pyramid_learning_journal:static')
+        config.scan()
+        return config.make_wsgi_app()
+
     app = main({})
+
     return TestApp(app)
+
+
+@pytest.fixture
+def db_session(configuration, request):
+    """."""
+    SessionFactory = configuration.registry['dbsession_factory']
+    session = SessionFactory()
+    engine = session.bind
+    Base.metadata.creat_all(engine)
+
+    def teardown():
+        session.transaction.rollback()
+        Base.metadata.drop_all(engine)
+
+    request.addfinalizer(teardown)
+    return session
+
+
+@pytest.fixture
+def dummy_request(db_session):
+    """."""
+    req = testing.DummyRequest()
+    req.dbsession = db_session
+    return req
+
+
+@pytest.fixture
+def post_request(dummy_request):
+    """."""
+    dummy_request.method = "POST"
+    return dummy_request
+
+
+@pytest.fixture
+def get_request(dummy_request):
+    """."""
+    dummy_request.method = "GET"
+    return dummy_request
+
+
+# got to here.
 
 
 @pytest.fixture
 def home_response():
     """Set fixture for home page."""
-    from pyramid_learning_journal.views.default import list_view
     request = testing.DummyRequest()
     response = list_view(request)
     return response
@@ -25,7 +91,6 @@ def home_response():
 @pytest.fixture
 def new_entry_response():
     """Set fixture for new entry page."""
-    from pyramid_learning_journal.views.default import create_view
     request = testing.DummyRequest()
     response = create_view(request)
     return response
@@ -33,7 +98,6 @@ def new_entry_response():
 
 def test_home_view_page_is_home(home_response):
     """Test list view is routed to home page."""
-    from pyramid_learning_journal.views.default import list_view
     request = testing.DummyRequest()
     response = list_view(request)
     assert response['page'] is 'home'
@@ -56,28 +120,11 @@ def test_home_view_returns_proper_content(testapp):
     assert expected_text in str(html)
 
 
-def test_new_entry_view_page_is_create(new_entry_response):
-    """Test create view is routed to new entry page."""
-    from pyramid_learning_journal.views.default import create_view
-    request = testing.DummyRequest()
-    response = create_view(request)
-    assert response['page'] is 'create'
-
-
 def test_new_entry_view_returns_proper_content(testapp):
     """New entry view returns the actual content from the html."""
     response = testapp.get('/journal/new-entry')
     html = response.html
     expected_text = '<div class="large-6 columns"><h2>New Entry</h2></div>'
-    assert expected_text in str(html)
-
-
-def test_edit_entry_view_returns_proper_content(testapp):
-    """Edit entry view returns the actual content from the html."""
-    response = testapp.get('/journal/1/edit-entry')
-    html = response.html
-    assert html.find()
-    expected_text = '<div class="large-6 columns grade1"></div>'
     assert expected_text in str(html)
 
 
