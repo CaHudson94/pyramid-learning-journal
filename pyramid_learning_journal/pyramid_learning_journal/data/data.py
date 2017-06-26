d8ef3590b1735cb7f375ac4464c26c54ec239abf
Chris Hudson <c.ahudson84@yahoo.com>
2017-06-25 23:40:05 -0700

    added the last of class side entries manually.

--- pyramid_learning_journal/pyramid_learning_journal/data/data.py
+++ pyramid_learning_journal/pyramid_learning_journal/data/data.py
@@ -1,7 +1,7 @@
 """Text for past entries for pyramid learning journal."""
 
 posts = [
-    {'id': 0, 'title': 'Day 1 - Chris Hudson', 'edit_date': '',
+    {'id': 0, 'title': 'Week 1 Day 1 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Monday, 15 May, 2017, 1:39 pm',
      'body':
      '''Today is a first day, and like every first day of something it \
@@ -14,7 +14,7 @@ and information was all fairly straight forward and I felt nicely \
 comfortable when we got to touching on python. We will see what \
 happens with classmates and each day of new information. \
 #screams a little internally'''},
-    {'id': 1, 'title': 'Day 2 - Chris Hudson', 'edit_date': '',
+    {'id': 1, 'title': 'Week 1 Day 2 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Tuesday, 16 May, 2017, 3:46 pm',
      'body':
      '''Todays class had some fun notes for things for best practice that I needed \
@@ -22,7 +22,7 @@ and working with my group for the assignment today was good for getting \
 into the swing and learning what needs some work on how things will flow \
 in the course. Still would love a chance to really get to know more people \
 in the class better but that will come with time.'''},
-    {'id': 2, 'title': 'Day 3 - Chris Hudson', 'edit_date': '',
+    {'id': 2, 'title': 'Week 1 Day 3 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Wednesday, 17 May, 2017, 10:30 pm',
      'body':
      '''Today I learned the true importance of stepping through your code one line \
@@ -31,7 +31,7 @@ an edge case. I also got to spend some great time getting to know more of \
 the class a bit better and helping both my self and them learn and understand \
 some of the work for today. All in all a productive, \
 positive, and long day.'''},
-    {'id': 3, 'title': 'Day 4 - Chris Hudson', 'edit_date': '',
+    {'id': 3, 'title': 'Week 1 Day 4 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Friday, 19 May, 2017, 1:37 am',
      'body':
      '''Today was an interesting day it was long and challenging but I finally \
@@ -43,12 +43,12 @@ gear into the sprinting and running ahead of us. Meeting more of the class \
 has been somewhat slow going but I am getting there, I now hope to meet \
 and maybe even make friends with everyone by at least week 8 if not \
 sooner.'''},
-    {'id': 4, 'title': 'Day 5 - Chris Hudson', 'edit_date': '',
+    {'id': 4, 'title': 'Week 1 Day 5 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Saturday, 20 May, 2017, 12:10 am',
      'body':
      '''I learned to both read discussions and issue reports and to manage my \
 time better as losing points sucks.'''},
-    {'id': 5, 'title': 'Day 6 - Chris Hudson', 'edit_date': '',
+    {'id': 5, 'title': 'Week 2 Day 1 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Monday, 22 May, 2017, 10:52 pm',
      'body':
      '''We got our new groups today which was exciting I am looking forward to \
@@ -58,7 +58,7 @@ from the past work. Other than that we learned plenty about servers and \
 classes today, I knew about classes before bu am apparently a lot worse \
 and or rustier with them than I thought. Over all a long day but a good \
 step into the week.'''},
-    {'id': 6, 'title': 'Day 7 - Chris Hudson', 'edit_date': '',
+    {'id': 6, 'title': 'Week 2 Day 2 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Tuesday, 23 May, 2017, 10:03 pm',
      'body':
      '''Today went well, work flow was fairly efficient and I felt like I had a \
@@ -66,7 +66,7 @@ better grasp of most of the concepts. Learned a lot more about inheritance \
 vs. composition and how to properly use it (surprisingly simpler than my \
 brain made it out to be). Working with my groups is a pleasure and Morgan \
 and I had some fun moments.'''},
-    {'id': 7, 'title': 'Day 8 - Chris Hudson', 'edit_date': '',
+    {'id': 7, 'title': 'Week 2 Day 3 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Thursday, 25 May, 2017, 12:34 am',
      'body':
      '''Day by day more and more events happen that people go to and say I should \
@@ -78,7 +78,7 @@ and concerned with finishing the work at hand. Learned about big O notation \
 in class and had a fun little gist, sadly it was a reminder that I am \
 apparently a very slow worker still. All things considered it was a \
 productive and informative day while also being fairly hard.'''},
-    {'id': 8, 'title': 'Day 9 - Chris Hudson', 'edit_date': '',
+    {'id': 8, 'title': 'Week 2 Day 4 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Friday, 26 May, 2017, 8:38 am',
      'body':
      '''Today was very productive while also still failing to start on the \
@@ -88,7 +88,7 @@ do before Monday and what was added today looks rather daunting on the \
 server side. I really enjoyed the amount we dove into the server \
 interactions in class but am also kind of sad it ate away at time for what \
 had been planned but such is life.'''},
-    {'id': 9, 'title': 'Day 10 - Chris Hudson', 'edit_date': '',
+    {'id': 9, 'title': 'Week 2 Day 5 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Saturday, 27 May, 2017, 9:33 pm',
      'body':
      '''Well a little late on this one doing the Friday ones is a little odd and I \
@@ -97,7 +97,7 @@ using composition, but I would love some good opportunities to use \
 inheritance as well. This week has been good, a lot of new information \
 coming in but I feel like I am processing it fairly well. The server side \
 stuff was definitely the harder half but been able to get through it.'''},
-    {'id': 10, 'title': 'Day 11 - Chris Hudson', 'edit_date': '',
+    {'id': 10, 'title': 'Week 3 Day 1 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Tuesday, 30 May, 2017, 10:32 pm',
      'body':
      '''Working with Erik today was fun and informative he has much more \
@@ -106,7 +106,7 @@ on fairly even footing with the class in general. Learning more and what I \
 have learned is growing less daunting even while what is coming in new is \
 still hitting quite hard. Managing dead lines has been kind of weird today \
 with the 3 day weekend and not being sure what assignments are due when.'''},
-    {'id': 11, 'title': 'Day 12 - Chris Hudson', 'edit_date': '',
+    {'id': 11, 'title': 'Week 3 Day 2 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Thursday, 1 June, 2017, 7:20 am',
      'body':
      '''We got our groups for project week today and I am pretty happy with who I \
@@ -118,7 +118,7 @@ ones seem easier and simpler solutions reveal themselves to them as we go \
 even if the new ones seem overtly more and more complicated. The server side \
 of things still seems and feels like the harder portion of things although \
 it still mostly makes sense, I just suck at anything front end.'''},
-    {'id': 12, 'title': 'Day 13 - Chris Hudson', 'edit_date': '',
+    {'id': 12, 'title': 'Week 3 Day 3 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Thursday, 1 June, 2017, 9:05 pm',
      'body':
      '''Today I learned that 4 day weeks suck they have all the madness and \
@@ -126,39 +126,139 @@ content of a 5 day week but with one less day before everything for that \
 week is done. I like having that time to process and this just feels like \
 more pressure to perform. I felt pretty lost today as far as direction \
 although the material made sense it was just a weird day.'''},
-    {'id': 13, 'title': 'Day 14 - Chris Hudson', 'edit_date': '',
+    {'id': 13, 'title': 'Week 3 Day 4 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Saturday, 3 June, 2017, 2:41 am',
      'body':
      '''Today was difficult in that there were so many things that needed \
-     to be done and needed attention that we couldn't possibly cover \
-     everything. Luckily I felt a lot more productive today (at least for \
-     the earlier half) than yesterday. We will see what the week end \
-     brings but I hope to get a little down time this time around.'''},
-    {'id': 14, 'title': 'Day 15 - Chris Hudson', 'edit_date': '',
+to be done and needed attention that we couldn't possibly cover \
+everything. Luckily I felt a lot more productive today (at least for \
+the earlier half) than yesterday. We will see what the week end \
+brings but I hope to get a little down time this time around.'''},
+    {'id': 14, 'title': 'Week 4 Day 1 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Tuesday, 6 June, 2017, 6:57 am',
      'body':
      '''Today was a catch up day and I got some more clarity on elements \
-     of the pyramid framework we have been working with, which is nice. \
-     Was getting a little sick though so that also timed out well, will \
-     work through the rest of it so I am not dragging my project team or \
-     groups for assignments.'''},
-    {'id': 15, 'title': 'Day 16 - Chris Hudson', 'edit_date': '',
+of the pyramid framework we have been working with, which is nice. \
+Was getting a little sick though so that also timed out well, will \
+work through the rest of it so I am not dragging my project team or \
+groups for assignments.'''},
+    {'id': 15, 'title': 'Week 4 Day 2 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Wednesday, 7 June, 2017, 11:59 am',
      'body':
      '''Today we learned about graph traversal and security. I think \
-     security is pretty cool as I have always enjoyed cryptography to \
-     a certain degree, math and all that, and it is a fairly simple and \
-     understandable way to secure data. I would be interested to learn \
-     more on how exactly the salt is used in the layering of the \
-     cryptogram. The concept of traversal seems fairly easy but being \
-     sick made it friarly hard to actually figure out how to do. I \
-     feel really sorry for my team mates and for missing class.'''},
-    {'id': 16, 'title': 'Day 17 - Chris Hudson', 'edit_date': '',
+security is pretty cool as I have always enjoyed cryptography to \
+a certain degree, math and all that, and it is a fairly simple and \
+understandable way to secure data. I would be interested to learn \
+more on how exactly the salt is used in the layering of the \
+cryptogram. The concept of traversal seems fairly easy but being \
+sick made it friarly hard to actually figure out how to do. I \
+feel really sorry for my team mates and for missing class.'''},
+    {'id': 16, 'title': 'Week 4 Day 3 - Chris Hudson', 'edit_date': '',
      'creation_date': 'Thursday, 8 June, 2017, 8:34 am',
      'body':
      '''Today I was out sick but we learned about adding layers to \
-     our security to prevent hacking via CSRF, adding weight to \
-     graphs, and more about how decorators work. I found decorators \
-     interesting as the layering of function calls fairly simple and \
-     straightforward while also accomplishing potentially a lot.'''},
+our security to prevent hacking via CSRF, adding weight to \
+graphs, and more about how decorators work. I found decorators \
+interesting as the layering of function calls fairly simple and \
+straightforward while also accomplishing potentially a lot.'''},
+    {'id': 17, 'title': 'Week 4 Day 4 - Chris Hudson', 'edit_date': '',
+     'creation_date': 'Thursday, 8 June, 2017, 9:21 pm',
+     'body':
+     '''Today we learned about Ajax in class a bit and did a bunch \
+of helpful code review including some testing which is what I have \
+REALLY been needing as it is the thing I am most in the dark and \
+behind with. Hopefully I can catch up over the weekend on some of \
+this testing stuff but today went about as well as I could have \
+hoped with having been out sick. We also made some good head way \
+with project R&D.'''},
+    {'id': 18, 'title': 'Week 4 Day 5 - Chris Hudson', 'edit_date': '',
+     'creation_date': 'Friday, 9 June, 2017, 10:16 pm',
+     'body':
+     '''Today was spent working on resumes with Brandy and it was \
+quite informative. We got to spend a good chunk of time working on \
+our pitch and summaries and then made notes on each others. I feel \
+like I walked away from it with good new information but it made it \
+kind of difficult to switch gears back into coding initially.'''},
+    {'id': 19, 'title': 'Week 5 Day 1 - Chris Hudson', 'edit_date': '',
+     'creation_date': 'Tuesday, 13 June, 2017, 6:58 am',
+     'body':
+     '''Today was the first day of project week and things went \
+faster and smoother than I really expected. We ended up with a \
+largely working model by the end of the day with clear ideas on what \
+needed to happen moving forward and the ability to achieve most if \
+not all of our stretch goals.'''},
+    {'id': 20, 'title': 'Week 5 Day 2 - Chris Hudson', 'edit_date': '',
+     'creation_date': 'Wednesday, 14 June, 2017, 8:06 am',
+     'body':
+     '''We got some good work done today on our project but found a \
+major hole as well. There is still a good chunk of work to do with it \
+given that hole but the main functionality is coming along as far as \
+visualization and our web app. It is functional on all platforms which \
+is great but inaccurate, hopefully our next days work will help solve \
+this (I have some ideas but nothing concrete as of yet). Working together \
+has been good so far which is a relief as I worried a larger group \
+might get muddled down with some members not having enough to really do.'''},
+    {'id': 21, 'title': 'Week 5 Day 3 - Chris Hudson', 'edit_date': '',
+     'creation_date': 'Thursday, 15 June, 2017, 8:37 am',
+     'body':
+     '''Got a chance to go to my first meet up today which was a load of \
+fun. There were several talks about things that related to exactly what \
+our group has been working with for our project and people were relating \
+to some of our struggles. Got good valuable information and got to meet \
+some new people. We also made good headway on our project and it should \
+be well rounded for presentations.'''},
+    {'id': 22, 'title': 'Week 5 Day 4 - Chris Hudson', 'edit_date': '',
+     'creation_date': 'Friday, 16 June, 2017, 8:11 am',
+     'body':
+     '''We finished up things today and actually published to the chrome \
+store! The web app looks great and all our tests are passing, although \
+they feel light but that is a symptom of the small amount of functions \
+we actually needed. Some very small touch up details that I want to \
+address but otherwise everything looks good and feels ready to go. Had \
+some minor personal frustrations but I think it was just not maintaining \
+proper eat and sleep levels.'''},
+    {'id': 23, 'title': 'Week 5 Day 5 - Chris Hudson', 'edit_date': '',
+     'creation_date': 'Saturday, 17 June, 2017, 2:59 pm',
+     'body':
+     '''Today was the end of our first project week! Presentations went \
+well all around and I felt that we had a clean product to show. Switching \
+back into the class room will be interesting with the assignments that \
+are due Monday and the work that will start back up. As a first project \
+week goes I think it went better than I could have asked for while still \
+having some personal learning experiences for next time around. I also \
+learned a lot from some of the other groups and how they handled things.'''},
+    {'id': 24, 'title': 'Week 6 Day 1 - Chris Hudson', 'edit_date': '',
+     'creation_date': 'Tuesday, 20 June, 2017, 8:01 am',
+     'body':
+     '''We made it through the first half! Started learning about django \
+and binary search trees today and got new partners. I am pretty sad to not \
+be working with Morgan any more but am hopeful that the new groups will work \
+out. The search tree seems pretty straight forward although the solutions to \
+it are making me think in slightly less direct manners. Django feels a bit \
+more natural for some reason, might just be the structure we got from doing \
+pyramid first, but only time will tell.'''},
+    {'id': 25, 'title': 'Week 6 Day 2 - Chris Hudson', 'edit_date': '',
+     'creation_date': 'Tuesday, 20 June, 2017, 6:09 pm',
+     'body':
+     '''Expanding on the new django imager app in class today was fun and \
+I learned quite a bit. The traversals have a pretty big range of difficulty \
+which is frustrating but hopefully they will come together fairly easy. Over \
+all a fairly productive day with some resubmits and movement forward on \
+current assignments.'''},
+    {'id': 26, 'title': 'Week 6 Day 3 - Chris Hudson', 'edit_date': '',
+     'creation_date': 'Wednesday, 21 June, 2017, 10:17 pm',
+     'body':
+     '''The data structures were a lot of fun although challenging got the \
+traversals done and understood, with some struggling. Deletion should be \
+interesting but manageable. The django side of things is a bit more behind \
+but I think we can bust that out quickly once Casey is back to a healthy \
+state. All in all a good productive day.'''},
+    {'id': 27, 'title': 'Week 6 Day 5 - Chris Hudson', 'edit_date': '',
+     'creation_date': 'Friday, 23 June, 2017, 7:33 pm',
+     'body':
+     '''Last entry on the class site new ones will be on mine from here. \
+Today felt drained and slow I was still getting things but the majority \
+of my energy was wasted as my mind wanted to work on code while we were \
+doing career development. The deletion wrapped up nicely but I am feeling \
+a bit behind of django.'''},
 ]
