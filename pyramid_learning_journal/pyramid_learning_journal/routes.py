4ba784d1a8bc4cf3f79518df4d9b9e8d258a5941
Chris Hudson <c.ahudson84@yahoo.com>
2017-06-29 14:57:36 -0700

    fixed functionality.

--- pyramid_learning_journal/pyramid_learning_journal/routes.py
+++ pyramid_learning_journal/pyramid_learning_journal/routes.py
@@ -3,7 +3,8 @@
 
 def includeme(config):
     """Assign web url routes to the names of different pages."""
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('static', 'pyramid_learning_journal:static')
+    config.add_static_view('js', 'pyramid_learning_journal:static/js')
     config.add_route('home', '/')
     config.add_route('detail', '/journal/{id:\d+}')
     config.add_route('create', '/journal/new-entry')
